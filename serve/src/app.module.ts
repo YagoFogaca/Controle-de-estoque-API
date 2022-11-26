c3900481942cebc2b6fe18ebdc8dc6a16f331533
Yago Fogaça <yagofogaca20@gmail.com>
2022-11-26 16:36:21 -0300

    feat: criação e implementação do database

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/service/user.service';
+import { DataBaseModule } from './prisma/database.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [DataBaseModule],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
