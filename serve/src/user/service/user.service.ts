b46525ef5c6483646e8f0f280659c15f46cac932
Yago Fogaça <yagofogaca20@gmail.com>
2022-11-26 16:17:09 -0300

    feat: criação do service de usuario

---
+++ serve/src/user/service/user.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { IUserEntity } from '../entities/user.entity';
+import { PartialUserDto } from './dto/partialUser.dto';
+import { UserDto } from './dto/user.dto';
+
+@Injectable()
+export class UserService {
+  private mocksUser: IUserEntity[] = [];
+  constructor(private readonly repository) {}
+
+  async getAll(): Promise<IUserEntity[]> {
+    const user = await this.repository.getAll();
+    if (!user) {
+      throw new Error('Usuário não encontrado.');
+    }
+
+    return user;
+  }
+
+  async getById(id: string): Promise<IUserEntity> {
+    const user = await this.repository.getById(id);
+    if (!user) {
+      throw new Error('Usuário não encontrado.');
+    }
+
+    return user;
+  }
+
+  async create(user: UserDto): Promise<IUserEntity> {
+    const verifyEmail = await this.repository.getByEmail(user.email);
+    if (verifyEmail) {
+      throw new Error('Email já registrado');
+    }
+
+    const verifyCpf = await this.repository.getByCpf(user.cpf);
+    if (verifyCpf) {
+      throw new Error('CPF ja registrado');
+    }
+
+    const userCreate = await this.repository.create(user);
+    return userCreate;
+  }
+
+  async update(user: PartialUserDto, id: string): Promise<IUserEntity> {
+    const userData = await this.getById(id);
+
+    const userUpdate = Object.assign(userData, user);
+    const userUpdated = await this.repository.update(userUpdate);
+    if (!userUpdated) {
+      throw new Error('Erro ao atualizar o usuário.');
+    }
+
+    return userUpdated;
+  }
+
+  async delete(id: string): Promise<IUserEntity> {
+    const user = await this.repository.delete(id);
+    if (!user) {
+      throw new Error('Usuário não encontrado.');
+    }
+
+    return user;
+  }
+}
