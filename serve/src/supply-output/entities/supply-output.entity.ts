92578bd61432d6dd259c3e5c6fffe4674a4ab0cb
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-03 19:21:28 -0300

    fix: renome entidade supply

--- serve/src/supply-output/entities/supply-output.entity.ts
+++ serve/src/supply-output/entities/supply-output.entity.ts
@@ -1,8 +1,8 @@
-import { Supply } from 'src/supplies/entities/supply.entity';
+import { ISupply } from 'src/supplies/entities/supply.entity';
 
 export class SupplyOutput {
   id: string;
-  id_supply: Supply;
+  id_supply: ISupply;
   name_supply: string;
   amount: number;
   unity: string;
