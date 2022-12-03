f05f0f968c59465dbd4d78e57c7ba52054bf91ef
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-03 19:23:02 -0300

    feat: criação do controller de supply

--- serve/src/supplies/supplies.controller.ts
+++ serve/src/supplies/supplies.controller.ts
@@ -1,34 +1,66 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SuppliesService } from './supplies.service';
 import { CreateSupplyDto } from './dto/create-supply.dto';
 import { UpdateSupplyDto } from './dto/update-supply.dto';
 
-@Controller('supplies')
+@Controller('/supplies')
 export class SuppliesController {
   constructor(private readonly suppliesService: SuppliesService) {}
 
-  @Post()
+  @Post('/create')
   create(@Body() createSupplyDto: CreateSupplyDto) {
     return this.suppliesService.create(createSupplyDto);
   }
 
-  @Get()
+  @Get('/get-all')
   findAll() {
     return this.suppliesService.findAll();
   }
 
-  @Get(':id')
+  @Get('/get-one/:id')
   findOne(@Param('id') id: string) {
-    return this.suppliesService.findOne(+id);
+    return this.suppliesService.findOne(id);
   }
 
-  @Patch(':id')
+  @Patch('/update/:id')
   update(@Param('id') id: string, @Body() updateSupplyDto: UpdateSupplyDto) {
-    return this.suppliesService.update(+id, updateSupplyDto);
+    return this.suppliesService.update(id, updateSupplyDto);
   }
 
-  @Delete(':id')
+  // Essas duas funções não teram endpoint, somente para entry e output
+
+  @Patch('/update-output/:id')
+  supplyOutput(
+    @Param('id') id: string,
+    @Body() updateSupplyDto: UpdateSupplyDto,
+  ) {
+    return this.suppliesService.supplyOutput(
+      id,
+      updateSupplyDto.output_quantity,
+    );
+  }
+
+  @Patch('/update-input/:id')
+  supplyInput(
+    @Param('id') id: string,
+    @Body() updateSupplyDto: UpdateSupplyDto,
+  ) {
+    return this.suppliesService.supplyInput(
+      id,
+      updateSupplyDto.output_quantity,
+    );
+  }
+
+  @Delete('/delete/:id')
   remove(@Param('id') id: string) {
-    return this.suppliesService.remove(+id);
+    return this.suppliesService.remove(id);
   }
 }
