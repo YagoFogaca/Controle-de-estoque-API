b42772565d51c139c96f4b428f58c0b31b6c15dd
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-03 14:16:56 -0300

    feat: criação da entidade e dto de profiles

--- serve/src/profile/dto/create-profile.dto.ts
+++ serve/src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,9 @@
-export class CreateProfileDto {}
+import { IUserEntity } from 'src/user/entities/user.entity';
+
+export class CreateProfileDto {
+  user: IUserEntity;
+  title: string;
+  imageURL: string;
+  supply_output: string[];
+  supply_entry: string[];
+}
--- serve/src/profile/entities/profile.entity.ts
+++ serve/src/profile/entities/profile.entity.ts
@@ -1,1 +1,10 @@
-export class Profile {}
+import { IUserEntity } from 'src/user/entities/user.entity';
+
+export class Profile {
+  id: string;
+  user: IUserEntity;
+  title: string;
+  imageURL: string;
+  supply_output: string[];
+  supply_entry: string[];
+}
