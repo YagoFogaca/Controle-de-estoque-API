5768cf1ff7c10e48fe1211c383644e75726e7b1f
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-03 14:24:01 -0300

    feat: criação da entidade e dto de supply-output

--- serve/src/supply-output/dto/create-supply-output.dto.ts
+++ serve/src/supply-output/dto/create-supply-output.dto.ts
@@ -1,1 +1,9 @@
-export class CreateSupplyOutputDto {}
+export class CreateSupplyOutputDto {
+  id_supply: string;
+  name_supply: string;
+  amount: number;
+  unity: string;
+  requester: string;
+  date: Date;
+  time: Date;
+}
--- serve/src/supply-output/entities/supply-output.entity.ts
+++ serve/src/supply-output/entities/supply-output.entity.ts
@@ -1,1 +1,10 @@
-export class SupplyOutput {}
+export class SupplyOutput {
+  id: string;
+  id_supply: string;
+  name_supply: string;
+  amount: number;
+  unity: string;
+  requester: string;
+  date: Date;
+  time: Date;
+}
