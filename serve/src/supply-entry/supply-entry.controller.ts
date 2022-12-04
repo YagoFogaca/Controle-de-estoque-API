5e7b50eeabd7df4d08eb6b45f819b7ca38d15ca5
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-04 18:53:06 -0300

    feat: criação temporaria do service e controller de supply-entry

--- serve/src/supply-entry/supply-entry.controller.ts
+++ serve/src/supply-entry/supply-entry.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { SupplyEntryService } from './supply-entry.service';
 import { CreateSupplyEntryDto } from './dto/create-supply-entry.dto';
 import { UpdateSupplyEntryDto } from './dto/update-supply-entry.dto';
 
-@Controller('supply-entry')
+@Controller('/supply-entry')
 export class SupplyEntryController {
   constructor(private readonly supplyEntryService: SupplyEntryService) {}
 
-  @Post()
-  create(@Body() createSupplyEntryDto: CreateSupplyEntryDto) {
-    return this.supplyEntryService.create(createSupplyEntryDto);
+  @Post('/create')
+  create(
+    @Query('id_profile') id_profile: string,
+    @Body() createSupplyEntryDto: CreateSupplyEntryDto,
+  ) {
+    return this.supplyEntryService.create(createSupplyEntryDto, id_profile);
   }
 
-  @Get()
+  @Get('/find-all')
   findAll() {
     return this.supplyEntryService.findAll();
   }
 
-  @Get(':id')
+  @Get('/find-one/:id')
   findOne(@Param('id') id: string) {
-    return this.supplyEntryService.findOne(+id);
+    return this.supplyEntryService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSupplyEntryDto: UpdateSupplyEntryDto) {
-    return this.supplyEntryService.update(+id, updateSupplyEntryDto);
+  @Patch('/update/:id')
+  update(
+    @Param('id') id: string,
+    @Body() updateSupplyEntryDto: UpdateSupplyEntryDto,
+  ) {
+    return this.supplyEntryService.update(id, updateSupplyEntryDto);
   }
 
-  @Delete(':id')
+  @Delete('/delete/:id')
   remove(@Param('id') id: string) {
-    return this.supplyEntryService.remove(+id);
+    return this.supplyEntryService.remove(id);
   }
 }
--- serve/src/supply-entry/supply-entry.service.ts
+++ serve/src/supply-entry/supply-entry.service.ts
@@ -1,26 +1,81 @@
 import { Injectable } from '@nestjs/common';
+import { SuppliesService } from 'src/supplies/supplies.service';
+import { IdGenerator } from 'src/utils/id-generator/id-generator';
 import { CreateSupplyEntryDto } from './dto/create-supply-entry.dto';
 import { UpdateSupplyEntryDto } from './dto/update-supply-entry.dto';
+import { SupplyEntry } from './entities/supply-entry.entity';
+import { ProfileService } from '../profile/profile.service';
 
 @Injectable()
 export class SupplyEntryService {
-  create(createSupplyEntryDto: CreateSupplyEntryDto) {
+  private _supply_entry: SupplyEntry[] = [];
+
+  constructor(
+    private readonly SuppliesService: SuppliesService,
+    private readonly ProfileService: ProfileService,
+  ) {}
+
+  async create(
+    supplyEntry: CreateSupplyEntryDto,
+    id_profile: string,
+  ): Promise<string> {
+    // colcoar um entidade de validação dos campos
+    const createSupplyEntry = {
+      ...supplyEntry,
+      id: IdGenerator.idGenerator(),
+      id_profile,
+    };
+
+    // Verificação se o insumo existe e contabilização da quantidade de entrada daquele insumo
+    await this.SuppliesService.supplyInput(
+      createSupplyEntry.id_supply,
+      createSupplyEntry.amount,
+    );
+
+    // Verificação se o profile existe
+    await this.ProfileService.findOne(createSupplyEntry.id_profile);
+
+    this._supply_entry.push(createSupplyEntry);
     return 'This action adds a new supplyEntry';
   }
 
-  findAll() {
-    return `This action returns all supplyEntry`;
+  async findAll(): Promise<SupplyEntry[]> {
+    return this._supply_entry;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} supplyEntry`;
+  async findOne(id: string): Promise<SupplyEntry> {
+    const supplyEntry = this._supply_entry.find(
+      (supply_entry) => id === supply_entry.id,
+    );
+    if (!supplyEntry) {
+      throw new Error('A entrada de insumo não foi encontrado.');
+    }
+    return supplyEntry;
   }
 
-  update(id: number, updateSupplyEntryDto: UpdateSupplyEntryDto) {
-    return `This action updates a #${id} supplyEntry`;
+  async update(id: string, supplyEntry: UpdateSupplyEntryDto): Promise<string> {
+    // Colocar um verificação se a quatidade de entrada também foi alterada
+    const findOne = await this.findOne(id);
+
+    const updateSupplyEntry = Object.assign(findOne, supplyEntry);
+    this._supply_entry.map((findSupply, index) => {
+      if (findSupply.id === id) {
+        this._supply_entry.splice(index, 1, updateSupplyEntry);
+      }
+    });
+    return 'Entrada de insumo atualizada com sucesso';
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} supplyEntry`;
+  async remove(id: string): Promise<string> {
+    this._supply_entry.map((supplyEntry, index) => {
+      if (supplyEntry.id === id) {
+        this._supply_entry.splice(index, 1);
+        this.SuppliesService.supplyOutput(
+          supplyEntry.id_supply,
+          supplyEntry.amount,
+        );
+      }
+    });
+    return 'Entrada de insumo deletada com sucesso';
   }
 }
