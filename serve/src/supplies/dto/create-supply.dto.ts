6bb4afcfdaec125ff2b122a38975e6b6596d8cce
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-06 22:51:41 -0300

    feat: Reestruturação de pastas

--- serve/src/supplies/dto/create-supply.dto.ts
+++
@@ -1,9 +0,0 @@
-import { UnitySupply } from '../../utils/enuns/enum';
-
-export class CreateSupplyDto {
-  name: string;
-  unity: UnitySupply;
-  quantity_stock: number;
-  output_quantity: number;
-  active: boolean;
-}
