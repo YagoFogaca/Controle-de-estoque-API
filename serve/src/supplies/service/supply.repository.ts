0d45bc0033ca815e57ea8bb02a23ff8ed8618620
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-06 22:53:14 -0300

    feat: criação do repository de supply

---
+++ serve/src/supplies/service/supply.repository.ts
@@ -0,0 +1,36 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Supply } from '../entities/supply.entity';
+import { UpdateSupplyDto } from './dto/update-supply.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SupplyRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(supply: Supply): Promise<Supply> {
+    return await this.prismaService.supply.create({ data: supply });
+  }
+
+  async findAll(): Promise<Supply[]> {
+    return await this.prismaService.supply.findMany();
+  }
+
+  async findById(id: string): Promise<Supply> {
+    return await this.prismaService.supply.findFirst({ where: { id: id } });
+  }
+
+  async findByName(name: string): Promise<Supply> {
+    return await this.prismaService.supply.findFirst({ where: { name: name } });
+  }
+
+  async update(id: string, supply: UpdateSupplyDto): Promise<Supply> {
+    return await this.prismaService.supply.update({
+      where: { id: id },
+      data: supply,
+    });
+  }
+
+  async delete(id: string): Promise<Supply> {
+    return await this.prismaService.supply.delete({ where: { id: id } });
+  }
+}
