3ddcebf5200c98808b93c82efcd5421d3b4873e7
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-06 22:51:01 -0300

    feat: atualização do dto de criação e atualização de supply

---
+++ serve/src/supplies/service/dto/create-supply.dto.ts
@@ -0,0 +1,9 @@
+import { UnitySupply } from '../../../utils/enuns/enum';
+
+export class CreateSupplyDto {
+  name: string;
+  unity: UnitySupply;
+  quantity_stock: number;
+  output_quantity: number;
+  active: boolean;
+}
