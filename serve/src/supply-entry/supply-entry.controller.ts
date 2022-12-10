722d8d5105efbf901e9dbbf6bdcea4de2e884e8c
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-10 14:44:45 -0300

    feat: atualização do controller para receber o service

--- serve/src/supply-entry/supply-entry.controller.ts
+++ serve/src/supply-entry/supply-entry.controller.ts
@@ -17,33 +17,37 @@ export class SupplyEntryController {
   constructor(private readonly supplyEntryService: SupplyEntryService) {}
 
   @Post('/create')
-  create(
+  async create(
     @Query('id_profile') id_profile: string,
     @Body() createSupplyEntryDto: CreateSupplyEntryDto,
   ) {
-    return this.supplyEntryService.create(createSupplyEntryDto, id_profile);
+    console.log(id_profile);
+    return await this.supplyEntryService.create(
+      createSupplyEntryDto,
+      id_profile,
+    );
   }
 
   @Get('/find-all')
-  findAll() {
-    return this.supplyEntryService.findAll();
+  async findAll() {
+    return await this.supplyEntryService.findAll();
   }
 
-  @Get('/find-one/:id')
-  findOne(@Param('id') id: string) {
-    return this.supplyEntryService.findOne(id);
-  }
+  // @Get('/find-one/:id')
+  // findOne(@Param('id') id: string) {
+  //   return this.supplyEntryService.findOne(id);
+  // }
 
-  @Patch('/update/:id')
-  update(
-    @Param('id') id: string,
-    @Body() updateSupplyEntryDto: UpdateSupplyEntryDto,
-  ) {
-    return this.supplyEntryService.update(id, updateSupplyEntryDto);
-  }
+  // @Patch('/update/:id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateSupplyEntryDto: UpdateSupplyEntryDto,
+  // ) {
+  //   return this.supplyEntryService.update(id, updateSupplyEntryDto);
+  // }
 
-  @Delete('/delete/:id')
-  remove(@Param('id') id: string) {
-    return this.supplyEntryService.remove(id);
-  }
+  // @Delete('/delete/:id')
+  // remove(@Param('id') id: string) {
+  //   return this.supplyEntryService.remove(id);
+  // }
 }
