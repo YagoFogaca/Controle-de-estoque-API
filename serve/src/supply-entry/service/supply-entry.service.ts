e049ae89eb6ffc0429dee7d41311374c170e51ca
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-10 14:44:21 -0300

    feat: atualização do service para receber o usecase

--- serve/src/supply-entry/service/supply-entry.service.ts
+++ serve/src/supply-entry/service/supply-entry.service.ts
@@ -5,77 +5,60 @@ import { CreateSupplyEntryDto } from './dto/create-supply-entry.dto';
 import { UpdateSupplyEntryDto } from './dto/update-supply-entry.dto';
 import { SupplyEntry } from '../entities/supply-entry.entity';
 import { ProfileService } from '../../profile/service/profile.service';
+import { CreateSupplyEntryUsecase } from './usecase/create.supply-entry';
+import { FindAllSupplyEntryUsecase } from './usecase/find-all.supply-entry';
 
 @Injectable()
 export class SupplyEntryService {
-  private _supply_entry: SupplyEntry[] = [];
-
   constructor(
-    private readonly SuppliesService: SuppliesService,
-    private readonly ProfileService: ProfileService,
+    private readonly createSupplyEntry: CreateSupplyEntryUsecase,
+    private readonly findAllSupplyEntryUsecase: FindAllSupplyEntryUsecase,
   ) {}
 
   async create(
     supplyEntry: CreateSupplyEntryDto,
     profileId: string,
   ): Promise<string> {
-    // colcoar um entidade de validação dos campos
-    const createSupplyEntry = {
-      ...supplyEntry,
-      id: IdGenerator.idGenerator(),
-      profileId,
-    };
-
-    // Verificação se o insumo existe e contabilização da quantidade de entrada daquele insumo
-    await this.SuppliesService.supplyInput(
-      createSupplyEntry.supplyId,
-      createSupplyEntry.amount,
-    );
-
-    // Verificação se o profile existe
-    await this.ProfileService.findOne(createSupplyEntry.supplyId);
-
-    this._supply_entry.push(createSupplyEntry);
-    return 'This action adds a new supplyEntry';
+    return await this.createSupplyEntry.execute(supplyEntry, profileId);
   }
 
   async findAll(): Promise<SupplyEntry[]> {
-    return this._supply_entry;
+    return this.findAllSupplyEntryUsecase.execute();
   }
 
-  async findOne(id: string): Promise<SupplyEntry> {
-    const supplyEntry = this._supply_entry.find(
-      (supply_entry) => id === supply_entry.id,
-    );
-    if (!supplyEntry) {
-      throw new Error('A entrada de insumo não foi encontrado.');
-    }
-    return supplyEntry;
-  }
+  // async findOne(id: string): Promise<SupplyEntry> {
+  //   const supplyEntry = this._supply_entry.find(
+  //     (supply_entry) => id === supply_entry.id,
+  //   );
+  //   if (!supplyEntry) {
+  //     throw new Error('A entrada de insumo não foi encontrado.');
+  //   }
+  //   return supplyEntry;
+  // }
 
-  async update(id: string, supplyEntry: UpdateSupplyEntryDto): Promise<string> {
-    // Colocar um verificação se a quatidade de entrada também foi alterada
-    const findOne = await this.findOne(id);
+  // async update(id: string, supplyEntry: UpdateSupplyEntryDto): Promise<string> {
+  //   // Colocar um verificação se a quatidade de entrada também foi alterada
+  //   const findOne = await this.findOne(id);
 
-    const updateSupplyEntry = Object.assign(findOne, supplyEntry);
-    this._supply_entry.map((findSupply, index) => {
-      if (findSupply.id === id) {
-        this._supply_entry.splice(index, 1, updateSupplyEntry);
-      }
-    });
-    return 'Entrada de insumo atualizada com sucesso';
-  }
+  //   const updateSupplyEntry = Object.assign(findOne, supplyEntry);
+  //   this._supply_entry.map((findSupply, index) => {
+  //     if (findSupply.id === id) {
+  //       this._supply_entry.splice(index, 1, updateSupplyEntry);
+  //     }
+  //   });
+  //   return 'Entrada de insumo atualizada com sucesso';
+  // }
 
-  async remove(id: string): Promise<string> {
-    this._supply_entry.map((supplyEntry, index) => {
-      if (supplyEntry.id === id) {
-        this._supply_entry.splice(index, 1);
-        this.SuppliesService.supplyOutput(
-          supplyEntry.supplyId,
-          supplyEntry.amount,
-        );
-      }
-    });
-    return 'Entrada de insumo deletada com sucesso';
-  }
+  // async remove(id: string): Promise<string> {
+  //   this._supply_entry.map((supplyEntry, index) => {
+  //     if (supplyEntry.id === id) {
+  //       this._supply_entry.splice(index, 1);
+  //       this.SuppliesService.supplyOutput(
+  //         supplyEntry.supplyId,
+  //         supplyEntry.amount,
+  //       );
+  //     }
+  //   });
+  //   return 'Entrada de insumo deletada com sucesso';
+  // }
 }
