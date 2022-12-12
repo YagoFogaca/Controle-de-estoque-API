5e412bbe3584430e3bc10915a4a716cff02fd45c
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-11 22:52:38 -0300

    feat: implementação do swagger e class-validator no CreateSupplyEntryDto

--- serve/src/supply-entry/service/dto/create-supply-entry.dto.ts
+++ serve/src/supply-entry/service/dto/create-supply-entry.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateSupplyEntryDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   supplyId: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   profileId: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
   amount: number;
 }
