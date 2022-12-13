a73d30ddc520e9851796e01bda3293bc65d234e5
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-13 13:25:28 -0300

    feat:  remoção de supply-output

--- serve/src/app.module.ts
+++ serve/src/app.module.ts
@@ -2,13 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProfileModule } from './profile/profile.module';
 import { SupplyEntryModule } from './supply-entry/supply-entry.module';
-import { SupplyOutputModule } from './supply-output/supply-output.module';
 import { SuppliesModule } from './supplies/supplies.module';
 import { AuthModule } from './auth/auth.module';
-
-// @Module({
-//   imports: [UserModule, ProfileModule, SupplyEntryModule, SupplyOutputModule, SuppliesModule],
-// })
 
 @Module({
   imports: [
