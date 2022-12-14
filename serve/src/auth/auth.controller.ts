97bcca51082c8c57fb66cff509e0db5d51ce4c6f
Yago Fogaça <yagofogaca20@gmail.com>
2022-12-14 07:42:54 -0300

    feat: remoção do metodo de teste de rotas autenticadas

--- serve/src/auth/auth.controller.ts
+++ serve/src/auth/auth.controller.ts
@@ -1,30 +1,16 @@
 import { Controller } from '@nestjs/common';
-import { Body, Get, Post, Req, UseGuards } from '@nestjs/common/decorators';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { UserService } from 'src/user/service/user.service';
+import { Body, Post } from '@nestjs/common/decorators';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
-import { Request } from 'express';
-import { IsAdminAuthorization } from './decorators/admin.decorator';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
   async login(@Body() user: AuthDto) {
     return await this.authService.validateUser(user);
-  }
-
-  @Get()
-  @UseGuards(AuthGuard(), IsAdminAuthorization)
-  @ApiBearerAuth()
-  async getUser(@Req() req: Request) {
-    return await this.userService.getAll();
   }
 }
